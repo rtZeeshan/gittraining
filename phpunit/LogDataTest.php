54e1f411ada1f8dc133b452e02ef53088e4db73f
Zeeshan Awan <zeeshan.awan@RT-DT-501.rolustech.net>
2016-08-18 12:05:17 +0500

    testing logic hook and relationship with sugar phpunit which i would like to discuss more

---
+++ phpunit/LogDataTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * Copyright 2015 SugarCRM Inc.
+ */
+
+require_once 'custom/modules/Contacts/log_data.php';
+
+/**
+ *  Example tests for our custom Logic Hook.
+ */
+class LogDataTest extends Sugar_PHPUnit_Framework_TestCase {
+
+    private $bean; //Accounts bean
+    private $hooks; //Hooks class
+    private $rel_arr;
+    /**
+     * Set up before each test
+     */
+
+    public function setUp() {
+        parent::setUp();
+        // $abc = BeanFactory::getBean('Contacts','8a77114c-c768-71ca-2305-57ac0055e914');
+
+        $this->hooks = $this->getMockBuilder(logData::class)->getMock();
+        //$this->g
+        //$this->hooks = new logData();
+        $this->hooks->method('loggingData')->willReturn('qwe');
+
+        $this->bean = $this->getMock("Contact");
+        $this->bean->id = '8a77114c-c768-71ca-2305-57ac0055e914';
+        $this->bean->last_name = 'qwe';
+
+        $schoo = $this->getMockBuilder(sch_schools::class)->getMock();
+        $this->rel_arr = array();
+        foreach ($schoo->field_defs as $field => $def) {
+            if (isset($def['relationship'])) {
+                //return(array($def['name']));
+                $this->rel_arr[] = $def['relationship'];
+                // $GLOBALS['log']->fatal($def['relationship']);
+            }
+
+            //   $GLOBALS['log']->fatal(print_r($schoo->field_defs,true));
+            /**
+             * Use SugarTestHelper to set up only those Sugar global values that are needed.
+             * Framework will tear these down automatically after each test.
+             */
+            // SugarTestHelper::setUp("beanList");
+        }
+       // $GLOBALS['log']->fatal(print_r($schoo, true));
+    }
+
+    /**
+     * Example that relies on SugarTestHelper
+     */
+    /*
+      public function testBeanListLoaded(){
+      global $beanList;
+      $this->assertNotEmpty($beanList["Contacts"], "This test relies on Contacts bean.");
+      }
+     * 
+
+     */
+
+    /**
+     * Verify that logic hook changes Industry value when necessary
+     */
+    public function testHook() {
+        //   $id = $this->bean->fetched_row['id'];
+        //  $bean = $this->bean;
+        //  $GLOBALS['log']->fatal($this->bean->first_name);
+        // if(isset($bean->id)){
+        $this->hooks->loggingData($this->bean, "before_save", array());
+        $this->assertEquals($this->bean->last_name, $this->hooks->loggingData($this->bean, "before_save", array()));
+        //}
+    }
+    
+    public function testRelationship(){
+        
+         $this->assertContains('sch_staff_sch_schools',$this->rel_arr);
+    }
+
+}
