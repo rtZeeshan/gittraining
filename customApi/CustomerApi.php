3e02287225e873137850d4b01534e445a5b79b94
Zeeshan Awan <zeeshan.awan@RT-DT-501.rolustech.net>
2016-08-23 15:56:58 +0500

    with sugar bean

--- customApi/CustomerApi.php
+++ customApi/CustomerApi.php
@@ -15,7 +15,6 @@ class CustomerApi extends SugarApi {
         return array(
             'getCustomerInfo' => array(
                 'reqType' => 'GET',
-                'noLoginRequired' => true,
                 'path' => array('customerInfo'),
                 'pathVars' => array(''),
                 'method' => 'getCustomerInfo',
@@ -25,6 +24,14 @@ class CustomerApi extends SugarApi {
     }
 
     public function getCustomerInfo($api, $args) {
+        
+        //return $this->withSugarQuery();
+        return $this->withBean();
+    }
+
+    public function withSugarQuery() {
+        // with sugar query
+
         $json_arr = array();
 
         $query = new SugarQuery();
@@ -32,6 +39,7 @@ class CustomerApi extends SugarApi {
         $query->from(BeanFactory::getBean('Prospects'), array('team_security' => false));
         $targets_arr = $query->execute();
         $target_leads = new SplFixedArray(count($targets_arr));
+        $target_lead_contact = new SplFixedArray(count($targets_arr));
 
         for ($i = 0; $i < count($targets_arr); $i++) {
             $json_obj = array();
@@ -52,6 +60,10 @@ class CustomerApi extends SugarApi {
                 if ($target_leads[$i]['contact_id'] != null) {
 
                     $json_obj['contact_id'] = $target_leads[$i]['contact_id'];
+                    $contactBean = BeanFactory::retrieveBean('Contacts', $target_leads[$i]['contact_id'], array('disable_row_level_security' => true));
+                    $json_obj['contact_name'] = $contactBean->name;
+                    $contactBeanData = array('id' => $contactBean->id, 'name' => $contactBean->name);
+                    $target_lead_contact[$i] = $contactBeanData;
                 }
                 if ($target_leads[$i]['opportunity_id'] != null) {
 
@@ -62,22 +74,29 @@ class CustomerApi extends SugarApi {
 
                     $json_obj['account_id'] = $target_leads[$i]['account_id'];
                 }
-//$target_leads[$i] = BeanFactory::retrieveBean('Leads', $target->lead_id, array('disable_row_level_security' => true));
+                //$target_leads[$i] = BeanFactory::retrieveBean('Leads', $target->lead_id, array('disable_row_level_security' => true));
             } else {
                 $json_obj['converted'] = false;
             }
             $json_arr[] = $json_obj;
         }
+
+
+
         $query2 = new SugarQuery();
         $query2->select(array('id', 'name', 'opportunity_id', 'contact_id', 'account_id'));
         $query2->from(BeanFactory::getBean('Leads'), array('team_security' => false));
         $resLeads = $query2->execute();
         $leads_arr = array();
+
         foreach ($resLeads as $lead) {
-            if (!in_array($lead, $target_leads)) {
+            if (!$this->find_obj($lead, $target_leads)) {
                 $leads_arr[] = $lead;
             }
         }
+
+        // return $leads_arr;
+        $lead_to_contact_arr = new SplFixedArray(count($leads_arr));
 
         for ($i = 0; $i < count($leads_arr); $i++) {
             $json_obj = array();
@@ -115,20 +134,165 @@ class CustomerApi extends SugarApi {
         $resContacts = $query3->execute();
         $contacts_arr = array();
         foreach ($resContacts as $contact) {
-            if (!in_array($contact, $lead_to_contact_arr)) {
+            if (!$this->find_obj($contact, $lead_to_contact_arr) && !$this->find_obj($contact, $target_lead_contact)) {
                 $contacts_arr[] = $contact;
             }
         }
+        //return $lead_to_contact_arr;
         for ($i = 0; $i < count($contacts_arr); $i++) {
             $json_obj = array();
             $json_obj['contact_id'] = $contacts_arr[$i]['id'];
-            $json_obj['contact_name'] = $contacts_arr[$i]['name__first_name'] . ' ' . $leads_arr[$i]['name__last_name'];
+            $json_obj['contact_name'] = $contacts_arr[$i]['name__first_name'] . ' ' . $contacts_arr[$i]['name__last_name'];
 
             // $json_obj = json_encode($json_obj);
             $json_arr[] = $json_obj;
         }
 
         return $json_arr;
+    }
+
+    public function withBean() {
+        // with sugar bean
+
+        $json_arr = array();
+
+        $targetBean = BeanFactory::newBean('Prospects');
+        $targets_arr = $targetBean->get_full_list();
+
+        $target_leads = new SplFixedArray(count($targets_arr));
+        $target_lead_contact = new SplFixedArray(count($targets_arr));
+        for ($i = 0; $i < count($targets_arr); $i++) {
+            $json_obj = array();
+
+            $json_obj['target_id'] = $targets_arr[$i]->id;
+            $json_obj['target_name'] = $targets_arr[$i]->name;
+            if ($targets_arr[$i]->lead_id != null) {
+
+                $leadBean = BeanFactory::retrieveBean('Leads', $targets_arr[$i]->lead_id, array('disable_row_level_security' => true));
+                $leadBeanData = array('id' => $leadBean->id, 'name' => $leadBean->name, 'opportunity_id' => $leadBean->opportunity_id, 'contact_id' => $leadBean->contact_id, 'account_id' => $leadBean->account_id);
+                $target_leads[$i] = $leadBeanData;
+                $json_obj['converted'] = true;
+                $json_obj['lead_id'] = $target_leads[$i]['id'];
+                $json_obj['lead_name'] = $target_leads[$i]['name'];
+
+                if ($target_leads[$i]['contact_id'] != null) {
+
+                    $json_obj['contact_id'] = $target_leads[$i]['contact_id'];
+                    $contactBean = BeanFactory::retrieveBean('Contacts', $target_leads[$i]['contact_id'], array('disable_row_level_security' => true));
+                    $json_obj['contact_name'] = $contactBean->name;
+                    $contactBeanData = array('id' => $contactBean->id, 'name' => $contactBean->name);
+                    $target_lead_contact[$i] = $contactBeanData;
+                }
+                if ($target_leads[$i]['opportunity_id'] != null) {
+
+                    $json_obj['opportunity_id'] = $target_leads[$i]['opportunity_id'];
+                    $oppBean = BeanFactory::retrieveBean('Opportunities', $target_leads[$i]['opportunity_id'], array('disable_row_level_security' => true));
+                    $json_obj['opportunity_name'] = $oppBean->name;
+                }
+
+                if ($target_leads[$i]['account_id'] != null) {
+
+                    $json_obj['account_id'] = $target_leads[$i]['account_id'];
+                    $accountBean = BeanFactory::retrieveBean('Accounts', $target_leads[$i]['account_id'], array('disable_row_level_security' => true));
+                    $json_obj['account_name'] = $accountBean->name;
+                }
+                //$target_leads[$i] = BeanFactory::retrieveBean('Leads', $target->lead_id, array('disable_row_level_security' => true));
+            } else {
+                $json_obj['converted'] = false;
+            }
+            $json_arr[] = $json_obj;
+        }
+
+        $leadBean = BeanFactory::newBean('Leads');
+        $resLeads = $leadBean->get_full_list();
+        $leads = new SplFixedArray(count($resLeads));
+        for ($i = 0; $i < count($resLeads); $i++) {
+
+            $leadBeanData = array('id' => $resLeads[$i]->id, 'name' => $resLeads[$i]->name, 'opportunity_id' => $resLeads[$i]->opportunity_id, 'contact_id' => $resLeads[$i]->contact_id, 'account_id' => $resLeads[$i]->account_id);
+            $leads[$i] = $leadBeanData;
+        }
+
+        $leads_arr = array();
+        foreach ($leads as $lead) {
+
+            if (!$this->find_obj($lead, $target_leads)) {
+                $leads_arr[] = $lead;
+            }
+        }
+
+        $lead_to_contact_arr = new SplFixedArray(count($leads_arr));
+
+        for ($i = 0; $i < count($leads_arr); $i++) {
+            $json_obj = array();
+            $json_obj['lead_id'] = $leads_arr[$i]['id'];
+            $json_obj['lead_name'] = $leads_arr[$i]['name'];
+
+            if ($leads_arr[$i]['contact_id'] != null || $leads_arr[$i]['contact_id'] != "") {
+
+                $json_obj['converted'] = true;
+                $json_obj['contact_id'] = $leads_arr[$i]['contact_id'];
+                $contactBean = BeanFactory::retrieveBean('Contacts', $leads_arr[$i]['contact_id'], array('disable_row_level_security' => true));
+                $json_obj['contact_name'] = $contactBean->name;
+                $contactBeanData = array('id' => $contactBean->id, 'name' => $contactBean->name);
+
+                $lead_to_contact_arr[$i] = $contactBeanData;
+            } else {
+                $json_obj['converted'] = false;
+            }
+            if ($leads_arr[$i]['opportunity_id'] != null || $leads_arr[$i]['opportunity_id'] != "") {
+
+
+                $json_obj['opportunity_id'] = $leads_arr[$i]['opportunity_id'];
+                $oppBean = BeanFactory::retrieveBean('Opportunities', $leads_arr[$i]['opportunity_id'], array('disable_row_level_security' => true));
+                $json_obj['opportunity_name'] = $oppBean->name;
+            }
+            if ($leads_arr[$i]['account_id'] != null || $leads_arr[$i]['account_id'] != "") {
+
+                $json_obj['account_id'] = $leads_arr[$i]['account_id'];
+                $accountBean = BeanFactory::retrieveBean('Accounts', $leads_arr[$i]['account_id'], array('disable_row_level_security' => true));
+                $json_obj['account_name'] = $accountBean->name;
+            }
+            $json_arr[] = $json_obj;
+        }
+
+        $contactBean = BeanFactory::newBean('Contacts');
+        $resContacts = $contactBean->get_full_list();
+        $contacts = new SplFixedArray(count($resContacts));
+        for ($i = 0; $i < count($resContacts); $i++) {
+
+            $contactBeanData = array('id' => $resContacts[$i]->id, 'name' => $resContacts[$i]->name);
+            $contacts[$i] = $contactBeanData;
+        }
+
+        $contacts_arr = array();
+        foreach ($contacts as $contact) {
+
+            if (!$this->find_obj($contact, $lead_to_contact_arr) && !$this->find_obj($contact, $target_lead_contact)) {
+                $contacts_arr[] = $contact;
+            }
+        }
+
+
+        for ($i = 0; $i < count($contacts_arr); $i++) {
+            $json_obj = array();
+            $json_obj['contact_id'] = $contacts_arr[$i]['id'];
+            $json_obj['contact_name'] = $contacts_arr[$i]['name'];
+
+            $json_arr[] = $json_obj;
+        }
+
+        return $json_arr;
+    }
+
+    public function find_obj($obj, &$arr) {
+
+
+        for ($j = 0; $j < count($arr); $j++) {
+            if ($arr[$j]['id'] == $obj['id']) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
